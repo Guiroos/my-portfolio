f39d8b38a7fe2d780f25bee08e05bcdc58229580
guiroos <guilherme.roosr@gmail.com>
2022-05-27 17:19:49 -0300

    feat: render Components in App.js

--- portfolio-react-tailwind/src/App.js
+++ portfolio-react-tailwind/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Stacks from "./components/Stacks";
 
 function App() {
   return (
     <div className="App">
-      <h1>Portifolio</h1>
+      <Navbar />
+      <Home />
+      <About />
+      <Stacks />
+      <Projects />
+      <Contact />
     </div>
   );
 }
